37c32e307e77002bd3a6d57d2fd45979ad43cc72
rubanshiva <ruban@kaivale.com>
2023-09-07 13:39:47 +0530

    header and footer changes

--- src/components/MainHome/AdvanceFeature.js
+++ src/components/MainHome/AdvanceFeature.js
@@ -10,9 +10,9 @@ function AdvanceFeature() {
 
 
             <div className="section_title" data-aos="fade-in" data-aos-duration="1500" data-aos-delay="100">
-              <h2>Advaced features</h2>
-              <p>Lorem Ipsum is simply dummy text of the printing and typese tting indus orem Ipsum has beenthe standard
-                dummy text ever since.</p>
+              <h2>Advanced features</h2>
+              <p>Magic Media fraud and bot detection software ensures that we serve only quality advertising from quality publishers. We offer exclusive sources, an intuitive interface with granular, 
+                real time stats to help you get the most out of your campaigns and increase your ROI.</p>
             </div>
 
             <div className="row">
@@ -22,10 +22,8 @@ function AdvanceFeature() {
                     <img src="assets/images/new/Secure-data.svg" alt="image" />
                   </div>
                   <div className="text_info">
-                    <h3>Secure data</h3>
-                    <p>Lorem Ipsum is simply dummy text of the prin
-                      ting and type setting indus ideas orem Ipsum has beenthe standard dummy text ever since the when an
-                      type setting indus ideas own.</p>
+                    <h3>Easy Yet Powerful</h3>
+                    <p>Magic Media is finely tuned to provide a great experience and easy to use interface.</p>
                   </div>
                 </div>
               </div>
@@ -35,10 +33,8 @@ function AdvanceFeature() {
                     <img src="assets/images/new/Fully-functional.svg" alt="image" />
                   </div>
                   <div className="text_info">
-                    <h3>Fully functional</h3>
-                    <p>Simply dummy text of the printing and type se
-                      tting indus ideas orem Ipsum has beenthe stanard dummy text ever since the when an type setting
-                      indus iLorem Ipsum is.</p>
+                    <h3>Advanced Algorithms</h3>
+                    <p>Set it and forget it with our advanced planning tools. Years of ad selling experience in one easy to use system.</p>
                   </div>
                 </div>
               </div>
@@ -48,26 +44,21 @@ function AdvanceFeature() {
                     <img src="assets/images/new/Live-chat.svg" alt="image" />
                   </div>
                   <div className="text_info">
-                    <h3>Live chat</h3>
-                    <p>Lorem Ipsum is simply dummy text of the prin
-                      ting and type setting indus ideas orem Ipsum has beenthe standard dummy text ever since the when an
-                      type setting indus ideas own.</p>
+                    <h3>Deep Targeting</h3>
+                    <p>Find the users that you want to sell your products to without wasting your ad budget on non converting advertising.</p>
                   </div>
                 </div>
               </div>
-              <div className="col-lg-6 col-md-12" data-aos="fade-up" data-aos-duration="1500">
+              {/* <div className="col-lg-6 col-md-12" data-aos="fade-up" data-aos-duration="1500">
                 <div className="feature_block">
                   <div className="icon">
                     <img src="assets/images/new/24-7-Support.svg" alt="image" />
                   </div>
                   <div className="text_info">
-                    <h3>24-7 Support</h3>
-                    <p>Lorem Ipsum is simply dummy text of the prin
-                      ting and type setting indus ideas orem Ipsum has beenthe standard dummy text ever since the when an
-                      type setting indus ideas own.</p>
+                   
                   </div>
                 </div>
-              </div>
+              </div> */}
             </div>
 
           </div>
